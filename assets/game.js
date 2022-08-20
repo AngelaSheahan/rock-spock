b2c8c991bd5b6f89ca5a3dcdfcdec9131d4d93b5
AngelaSheahan <alouise_35@yahoo.co.uk>
2022-08-20 11:13:52 +0000

    javaScript const elements created

---
+++ assets/game.js
@@ -0,0 +1,11 @@
+/**
+ * Declare all DOM Elements 
+ * */
+
+const buttons = document.getElementsByClassName("controls");
+const playerScore = document.getElementById("player-score");
+const computerScore= document.getElementById("computer-score");
+const playerImage = document.getElementById("player-image");
+const computerImage = document.getElementById("computer-image");
+const messgaes = document.getElementById("messages");
+const choices = ["Rock", "Paper", "Scissors", "Lizard", "Spock"];
