9afb715dbaee0c37244ec6a41bbe02470079946c
AngelaSheahan <alouise_35@yahoo.co.uk>
2022-08-20 11:29:05 +0000

    add images to image folder and 2 functions to games.js file

--- assets/game.js
+++ assets/game.js
@@ -9,3 +9,18 @@ const playerImage = document.getElementById("player-image");
 const computerImage = document.getElementById("computer-image");
 const messgaes = document.getElementById("messages");
 const choices = ["Rock", "Paper", "Scissors", "Lizard", "Spock"];
+
+/**
+ * Add EventListeners to all Buttons
+ */
+
+for (let button of buttons) {
+    button.addEventListener("click" function() {
+        let playerChoice = this.getAttribute("data-choice");
+        playGame(playerChoice);
+    });
+}
+
+function playGame(playerChoice) {
+playerImage.src = `assets/images${choices[playerChoice]}.png`;
+}
